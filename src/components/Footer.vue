273b171bdd4e92c928b7058ab65df6c6715ed542
adilwoes <lidaseow@gmail.com>
2021-03-17 15:31:49 +0800

    Created the footer (bottom bar)

---
+++ src/components/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="tealseparator">
+
+        </div>
+        <div style="clear: both;">
+            <p class="alignleft">
+                <a class="padding">Home</a>
+                <a>Contact Us</a>
+            </p>
+            <p class="alignright">Copyright &#169; 2021 JobIt All rights reserved.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.tealseparator {
+    background-color: #0094B6;
+    height: 15px;
+}
+.padding {
+    padding-right: 120px;
+}
+.alignleft {
+    padding-left: 150px;
+    float: left;
+}
+.alignright {
+    float: right;
+    padding-right: 150px;
+    color: #D7DBDD;
+}
+</style>
