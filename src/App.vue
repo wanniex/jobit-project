4cc235652fd84231634bd98880a4f043356d1043
sdas04 <srimoyeedastidar@gmail.com>
2021-03-17 20:19:55 +0800

    Login and signup

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,15 @@
 <script>
 import Footer from './components/Footer.vue'
 import HomePage from './components/HomePage.vue'
+import SignupBtn from './components/SignupBtn.vue'
+import LoginBtn from './components/LoginBtn.vue'
 
 export default {
   components: {
     'foot' : Footer,
-    HomePage
+    HomePage,
+    'loginBtn': LoginBtn,
+    'signupBtn': SignupBtn
   }
 }
 </script>
