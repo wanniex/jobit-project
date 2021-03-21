6e3fe0d78cc6e8f53d23157b38d285812372b670
adilwoes <lidaseow@gmail.com>
2021-03-21 16:10:09 +0800

    Changed firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAkf2_2OF2QlZd3c07jovOjVD-mc2sI9P8",
-    authDomain: "jobit-1e302.firebaseapp.com",
-    projectId: "jobit-1e302",
-    storageBucket: "jobit-1e302.appspot.com",
-    messagingSenderId: "291901227772",
-    appId: "1:291901227772:web:69bcaf567f927fed75cd79",
-    measurementId: "G-MJWR8R96PJ"
-  };
+  apiKey: "AIzaSyDtr6cQZ-bMR-dibwF9m6eE3oS9Gc9S3b4",
+  authDomain: "jobit-38c72.firebaseapp.com",
+  projectId: "jobit-38c72",
+  storageBucket: "jobit-38c72.appspot.com",
+  messagingSenderId: "407951507326",
+  appId: "1:407951507326:web:18f29a4c16508568d07d2f",
+  measurementId: "G-W1SZ08LYHH"
+};
 
 firebase.initializeApp(firebaseConfig);
 var database = firebase.firestore();
