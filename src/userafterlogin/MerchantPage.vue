34f4c1440be00f2179ca1f9987d56bf5e91fe810
wanxianq <wanxianquek@gmail.com>
2021-04-02 12:56:52 +0800

    Updated and aligned MerchantPage styling

--- src/userafterlogin/MerchantPage.vue
+++ src/userafterlogin/MerchantPage.vue
@@ -7,8 +7,8 @@
         <h1>Redeem your points</h1>
       </b-row>
 
-     <b-row align-h="center" class="mt-4">
-        <h4 style="color:gray">Choose your merchant:</h4>
+      <b-row align-h="center" class="mt-4">
+        <h4 style="color: gray">Choose your merchant:</h4>
       </b-row>
 
       <b-row align-h="center" class="mt-5 mb-5">
@@ -24,54 +24,57 @@
         <b-col>
           <b-card-group deck>
             <b-card
-              img-src="https://oroinc.com/b2b-ecommerce/wp-content/uploads/sites/3/2019/07/fairprice-1500x1500-1.png"
-              img-alt="Card image"
-              img-top
-              img-height="500"
-          img-width="200"
-          fluid
+              body-class="d-flex flex-column"
+              fluid
+              header="Fairprice"
+              align="center"
             >
+              <!-- <b-card-text class="text-center">
+                Fairprice
+              </b-card-text> -->
 
-              <b-card-text class="text-center">
-                <h4>FairPrice</h4><br>
-              </b-card-text>
-              <b-button href="#" id="button" class="btn btn-primary mx-auto d-block">Select</b-button>
+              <b-img
+                src="https://oroinc.com/b2b-ecommerce/wp-content/uploads/sites/3/2019/07/fairprice-1500x1500-1.png"
+                fluid
+                class="mb-4"
+              ></b-img>
+
+              <b-button href="#" id="button" class="mt-auto">Select</b-button>
             </b-card>
-          </b-card-group>
-        </b-col>
 
-        <b-col>
-          <b-card-group deck>
             <b-card
-              img-src="https://i.imgur.com/fuUy3Ah.png"
+              fluid
+              body-class="d-flex flex-column"
+              header="Pizza Hut"
+              align="center"
+            >
+              <!-- <b-row class="mt-auto" align-h="center">
+                Pizza Hut
+                              img-src="https://i.imgur.com/fuUy3Ah.png"
               img-alt="Card image"
               img-top
-              img-height="500"
-          img-width="200"
-          fluid
-            >
-              <b-card-text class="text-center">
-                  <h4>Pizza Hut</h4><br>
-              </b-card-text>
-              <b-button href="#" id="button" class="btn btn-primary mx-auto d-block">Select</b-button>
+              </b-row> -->
+
+              <b-img
+                src="https://i.imgur.com/fuUy3Ah.png"
+                fluid
+                class="mb-4"
+              ></b-img>
+              <b-button href="#" id="button" class="mt-auto">Select</b-button>
             </b-card>
-          </b-card-group>
-        </b-col>
 
-        <b-col>
-          <b-card-group deck>
             <b-card
-              img-src="https://i.imgur.com/yfQYUIm.png"
-              img-alt="Card image"
-              img-top
-              img-height="500"
-          img-width="200"
+              fluid
+              body-class="d-flex flex-column"
+              header="Starbucks"
+              align="center"
             >
-
-              <b-card-text class="text-center">
-                  <h4>Starbucks Coffee</h4><br>
-              </b-card-text>
-              <b-button href="#" id="button" class="btn btn-primary mx-auto d-block">Select</b-button>
+              <b-img
+                src="https://i.imgur.com/yfQYUIm.png"
+                fluid
+                class="mb-4"
+              ></b-img>
+              <b-button href="#" id="button" class="mt-auto">Select</b-button>
             </b-card>
           </b-card-group>
         </b-col>
@@ -96,13 +99,13 @@ export default {
 
 
 <style scoped>
-
 #button {
   background-color: #87ebd3;
   color: #ffff;
   border: none;
   transition-duration: 0.4s;
   width: 200px;
+  align-self: center;
 }
 
 #button:hover {
