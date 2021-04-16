b96b7765885942fd341c6d89110a1bc4e4ddd923
kenjilim <samuelkenjilim1997@gmail.com>
2021-04-16 16:17:51 +0800

    change profile image of admin

--- src/adminpages/AdminAftLoginTopNav.vue
+++ src/adminpages/AdminAftLoginTopNav.vue
@@ -8,16 +8,16 @@
       />
     </b-navbar-brand>
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
+  
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item to="/AdminHomepage">Dashboard</b-nav-item>
-        <b-nav-item to="/AdminSubmitPage">Submit donation</b-nav-item>
+        <b-nav-item to="/AdminHomePage">Dash Board</b-nav-item>
+        <b-nav-item to="/AdminSubmitPage">Approve Donation</b-nav-item>
       </b-navbar-nav>
       
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-avatar href="#foo" src="https://placekitten.com/300/300" class="mt-2 mr-2 ml-2" size="3rem"></b-avatar>
+        <b-avatar href="#foo" v-bind:src="placeimg" class="mt-2 mr-2 ml-2" size="3rem"></b-avatar>
         <b-button pill @click="signout()" id="sobutton" class="mt-2 mr-2 ml-2">Sign Out</b-button>
       </b-navbar-nav>
     </b-collapse>
@@ -31,6 +31,11 @@ import fb from 'firebase'
 
 
 export default {
+  data() {
+    return {
+      placeimg: "",
+    }
+  },
   components: {
     // loginBtn: LoginBtn,
     // signupBtn: SignupBtn,
@@ -46,6 +51,12 @@ export default {
         this.$router.push('/');
       });
     },
+  },
+  created() {
+    this.uid = fb.auth().currentUser.uid;
+    fb.firestore().collection("partners").doc(this.uid).get().then(doc => {
+      this.placeimg = doc.data().imageURL;
+    })
   }
 };
 </script>
