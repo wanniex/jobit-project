0c8bea9759b791841acbc1dac455192bd38ff25f
wanxianq <wanxianquek@gmail.com>
2021-04-20 15:37:05 +0800

    Revert "Updated footer position for AdminConfirmPage"
    This reverts commit e461cbae18e66726a418b68657da6fc38ff11b46.

--- src/adminpages/AdminConfirmPage.vue
+++ src/adminpages/AdminConfirmPage.vue
@@ -75,7 +75,7 @@
       </b-row>
       <div style = "margin-bottom:50px;"/>
     </b-container>
-  <div style = "margin-bottom: 100px;"/>
+
     <AdminFooter style = "position: fixed; bottom:0; width: 100%;"></AdminFooter>
   </div>
 </template>
